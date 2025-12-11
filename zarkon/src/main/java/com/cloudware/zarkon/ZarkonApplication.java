971e20ba14e5b774eb01efe9bfd705692291c190
Juan Contreras <115199779+juan-contrerasM@users.noreply.github.com>
2025-12-11 16:09:47 -0500

    Se crea proyecto con spring boot

---
+++ zarkon/src/main/java/com/cloudware/zarkon/ZarkonApplication.java
@@ -0,0 +1,13 @@
+package com.cloudware.zarkon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZarkonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZarkonApplication.class, args);
+	}
+
+}
---
+++ zarkon/src/test/java/com/cloudware/zarkon/ZarkonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudware.zarkon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZarkonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
